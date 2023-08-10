2da234ce83e2987a7d1e727d21691add434dc44c
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-08-10 16:30:31 +0200

    Try different redirection

--- .github/workflows/kind.sh
+++ .github/workflows/kind.sh
@@ -8,7 +8,7 @@ command -v kind
 echo "bash"
 bash --version
 echo "check"
-if ! command -v kind &>/dev/null; then
+if ! command -v kind >/dev/null 2>&1; then
   echo "kind is not installed. Please see https://kind.sigs.k8s.io/"
   exit 1
 fi
