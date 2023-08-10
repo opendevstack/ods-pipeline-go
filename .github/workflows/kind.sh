fd20c38439f464c76a3a9914537d5ad68cc3c6fd
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-08-10 16:26:28 +0200

    try without redirect

--- .github/workflows/kind.sh
+++ .github/workflows/kind.sh
@@ -5,10 +5,10 @@ echo "which:"
 which kind
 echo "command"
 command -v kind
+echo "bash"
+bash --version
 echo "check"
-
-
-if ! command -v kind &> /dev/null; then
+if ! command -v kind; then
   echo "kind is not installed. Please see https://kind.sigs.k8s.io/"
   exit 1
 fi
