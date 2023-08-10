3c5a4a0a0b88795a64e662850bb9490dda266749
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-08-10 16:23:36 +0200

    Try different methods

--- .github/workflows/kind.sh
+++ .github/workflows/kind.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -o errexit
 
+echo "which:"
+which kind
+echo "command"
+command -v kind
+echo "check"
+
+
 if ! command -v kind &> /dev/null; then
   echo "kind is not installed. Please see https://kind.sigs.k8s.io/"
   exit 1
